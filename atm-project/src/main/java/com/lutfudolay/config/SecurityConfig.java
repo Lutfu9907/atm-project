75c541034ea67ff8ca10b4a8f9225cad9ecadf94
LUTFU <dolaylutfu@gmail.com>
2025-10-03 17:46:35 +0300

    updating security config

--- atm-project/src/main/java/com/lutfudolay/config/SecurityConfig.java
+++ atm-project/src/main/java/com/lutfudolay/config/SecurityConfig.java
@@ -31,8 +31,11 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/api/users/register", "/api/auth/login").permitAll()
-                .anyRequest().authenticated()
+            		.anyRequest().permitAll()
+            		
+            	//.requestMatchers("/api/users/register", "/api/auth/login").permitAll()
+                //.requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                //.anyRequest().authenticated()
             )
             .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
